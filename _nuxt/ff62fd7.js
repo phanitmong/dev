f83c94fadbc16538852da41b5b3fe499f602d3f5
mongphanit <ispolicyteamtube@gmail.com>
2023-09-27 08:53:52 +0700

    last

---
+++ _nuxt/ff62fd7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{352:function(e,o,c){"use strict";c.r(o),o.default={message:{welcome:"សូមស្វាគមន៍",home:"ទំព័រដើម",about:"អំពីខ្ញុំ",contact:"ទំនាក់ទំនង",resume:"ប្រវត្តិរូប",portfolio:"កម្មវិធីបង្ហាញការងារ",blog:"ប្លក់",service:"សេវាកម្ម",skills:"ជំនាញ",education:"ការអប់រំ",experience:"បទពិសោធន៍",projects:"គម្រោង"}}}}]);
---
+++ locales/en-US.js
@@ -0,0 +1,17 @@
+export default {
+    message: {
+        welcome: 'Welcome',
+        home: 'Home',
+        about: 'About',
+        contact: 'Contact',
+        resume: 'Resume',
+        portfolio: 'Portfolio',
+        blog: 'Blog',
+        service: 'Services',
+        skills: 'Skills',
+        education: 'Education',
+        experience: 'Experience',
+        projects: 'Projects',
+    }
+
+}
